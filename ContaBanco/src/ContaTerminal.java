8b9b15f5e749808cd5691e48d6efba66ca1025f2
Jonath Oliveira <akiles22a@gmail.com>
2024-05-23 15:47:16 -0300

    ProjetoContador

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,31 +2,30 @@ import java.io.Console;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        int numero;
-        String agencia;  
-        String nomeCliente;  
-        double saldo;  
-
+        String sigla;
+      
         Console console = System.console();
 
-        if (console == null) {
-            System.err.println("Console não disponível.");
-            System.exit(1);
+        System.out.println("Digite a sua Nota: ");
+        sigla = console.readLine();       
+
+       switch (sigla){
+        case "P":{
+            System.out.println("PEQUENO");
+            break;
         }
-
-        System.out.println("Digite a sua Agencia: ");
-        agencia = console.readLine();
-
-        System.out.println("Digite a sua conta: ");
-        numero = Integer.parseInt(console.readLine());       
-
-        System.out.println("Digite seu nome: ");
-        nomeCliente = console.readLine();
-
-        System.out.println("Digite o valor do deposito: ");
-        saldo = Double.parseDouble(console.readLine());
-
-        System.out.println("Olá" + " " + nomeCliente + " " + "Obrigado por criar uma conta em nosso banco, sua agência é : " + agencia + " " + "conta : " + numero + " " + "seu saldo e : " + saldo + " " + " Ja esta disponivel para saque !!");
+        case "M":{
+            System.out.println("MEDIO");
+            break;
+        }
+        case "G":{
+            System.out.println("GRANDE");
+            break;
+        }
+            
+          default:
+          System.out.println("INDEFINIDO");
+       }
       
     }
 }
